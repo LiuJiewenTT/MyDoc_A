f98df9621f164f9a172f4b1c201aa57c26c68dd1
刘杰文 <1635370493@qq.com>
2023-04-17 09:45:29 +0800

    rb

--- _plugins/tag_filter1.rb
+++ _plugins/tag_filter1.rb
@@ -15,8 +15,9 @@ module AllTagsFilter
       end
   
       tags = counts.keys
-      tags.reject { |t| t.empty? }
+      tags = tags.reject { |t| t.empty? }
         .map { |tag| { 'name' => tag, 'count' => counts[tag] } }
+        .sort{ |tag1, tag2| tag1['name'] <=> tag2['name'] }
         .sort { |tag1, tag2| tag2['count'] <=> tag1['count'] }
     end
   end
